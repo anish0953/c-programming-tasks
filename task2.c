a2f29c1aa899064d8ae652b5fa23a0ca35020fb2
anish0953 <54959443+anish0953@users.noreply.github.com>
2019-09-06 13:33:12 +0530

    Add files via upload
    c programming 
    cel to fah

---
+++ task2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    float cel,fah;
+
+    printf("enter the temperature in celcius");
+    scanf("%f",&cel);
+
+    fah=cel*9/5+32;
+
+    printf("THE TEMPERATURE IN FARHANITE IS : %F",fah);
+
+
+    getch();
+}
